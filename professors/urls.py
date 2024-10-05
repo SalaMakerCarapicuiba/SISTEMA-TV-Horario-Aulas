004ce34c63fa9c066c4d0a162a6af1d43dd13980
Lucas Passos da Silva <LucasPassosSilva2207@gmail.com>
2024-10-05 20:10:56 -0300

    20:10 // 05/10/2024 // CRUD Professor

--- professors/urls.py
+++ professors/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.listar_professores, name="listar_professores"),
     path('cadastrar/', views.cadastrar_professor, name='cadastrar_professor'),
-    path('deletar/<int:id>', views.deletar_professor, name='deletar_professor')
+    path('deletar/<int:id>', views.deletar_professor, name='deletar_professor'),
+    path('detalhes/<int:id>', views.detalhes_professor, name='detalhes_professor'),
+    path('editar_professor/<int:id>', views.editar_professor, name='editar_professor'),
 ]
