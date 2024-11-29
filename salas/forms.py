d84e3b69522fcd351981d7150dbc425691941aee
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2024-11-28 21:00:56 -0300

    Atualizações nos formulários e templates

--- salas/forms.py
+++ salas/forms.py
@@ -5,4 +5,13 @@ class SalaForm(forms.ModelForm):
     class Meta:
         model = Sala
         fields = ['numero', 'materias']
-        exclude = ['materias']
+        # exclude = ['materias']
+        widgets = {
+              'numero': forms.TextInput(attrs={
+                'class': 'w-full p-2 border border-gray-300 rounded-lg bg-transparent text-custom1 placeholder-gray-400',
+                'placeholder': 'Ex: 101'
+            }),
+             'materias': forms.CheckboxSelectMultiple(attrs={
+                'class': 'space-y-2 text-custom1',
+            }),
+            }
