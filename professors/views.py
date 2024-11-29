c905a815729d5353f12acb72ce08df2b5eae51b5
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2024-11-28 21:50:29 -0300

    Removidas as seções de matérias do formulário de cadastro e edição de professores. Adicionada a exibição das matérias associadas a um professor na página de detalhes do professor.

--- professors/views.py
+++ professors/views.py
@@ -3,6 +3,7 @@ from .forms import ProfessorEditarForm, ProfessorForm
 from django.contrib.auth.decorators import login_required
 from .models import Professor
 from django.contrib import messages
+from materias.models import Materia
 
 #@login_required
 def cadastrar_professor(request):
@@ -32,10 +33,13 @@ def deletar_professor(request, id):
 
 def detalhes_professor(request, id):
     professor = get_object_or_404(Professor, id=id)
-    return render(request, 'professors/detalhes_professor.html', {'professor': professor})
+    materias = Materia.objects.filter(professor_id=professor.id)
+
+    return render(request, 'professors/detalhes_professor.html', {'professor': professor, 'materias': materias})
 
 def editar_professor(request, id):
-    professor = get_object_or_404(Professor, id=id)  # Pega o professor pelo id
+    professor = get_object_or_404(Professor, id=id)  # Pega o professor pelo id\
+
     if request.method == 'POST':
         form = ProfessorEditarForm(request.POST, instance=professor)  # Preenche o form com os dados submetidos
         if form.is_valid():
